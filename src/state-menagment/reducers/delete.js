1d5c72087634d143c3a8ce3a61b601239fe93b2c
narek.ghukasyan <narek.ghukasyan@betconstruct.com>
2021-03-01 14:20:31 +0400

    optimazie component

--- src/state-menagment/reducers/delete.js
+++
@@ -1,5 +0,0 @@
-import {types} from "../types";
-import createAsyncReducer from '../helpers/asyncReducer';
-
-const initialState = {};
-export default createAsyncReducer(types.REMOVE_TODO, initialState);
